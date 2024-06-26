1cca4711069163ea51758130038af90409d3143a
Paulo Eduardo <marbelinhameg1@gmail.com>
2024-06-26 14:37:44 -0300

    teste1

--- inicial.py
+++ inicial.py
@@ -1,7 +1,19 @@
-print("Bem vindo á programação")
-nome = input("qual é seu nome")
-print("ola",nome)
 
-print("Paulo")
-print("endereço: Rua tica ticao tiquinha")
-print(17)
+
+"Atividade 1"
+
+nome1 = input("Qual é seu nome")
+Apelido = input("Qual é seu Apelido")
+Endereco = input("Qual ´seu endereço")
+cidade = input("Qual é sua cidade e cep")
+print(nome1,Apelido,Endereco,cidade)
+
+"Atividade 2"
+
+nome2 = input("Digite um nome")
+ano = input("Digite um ano")
+print(nome2,"é uma valente cavaleira, nascida no ano de",ano,"Certa manha",nome2,
+      "acordou com um barulho terrivel: um dragão se aproximava da aldeia. Somente",nome2,"Poderia salvar os moradores da aldeia")
+
+
+"Atividade 3"
